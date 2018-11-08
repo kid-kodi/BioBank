8c5d82c72e36349a3be32405abc8aa93bbb57f50
kid-kodi <konedangui@gmail.com>
2018-11-08 12:23:04 +0000

    v.0.0.5

--- boot.sh
+++ boot.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# this script is used to boot a Docker container
 source venv/bin/activate
+flask db upgrade
 flask translate compile
-exec gunicorn -b :5000 --access-logfile - --error-logfile - biobank:app
+exec gunicorn -b :5000 --access-logfile - --error-logfile - run:app
